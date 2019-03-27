b16a518773e4b2f539a881259182fc657438ab98
Sharang Malhotra <sharang.2795@gmail.com>
2019-03-27 12:36:15 +0530

    react Beer app

---
+++ precache-manifest.55f278e462142fc0643da91a5bd4fc01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cc5ee2c2712090ea79aa",
+    "url": "/static/js/main.478c39da.chunk.js"
+  },
+  {
+    "revision": "37a09e250f8f44e688aa",
+    "url": "/static/js/2.071997d4.chunk.js"
+  },
+  {
+    "revision": "cc5ee2c2712090ea79aa",
+    "url": "/static/css/main.9021c040.chunk.css"
+  },
+  {
+    "revision": "849d0858b1ac687d17b704aa548b5aba",
+    "url": "/index.html"
+  }
+];
